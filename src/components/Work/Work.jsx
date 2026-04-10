a74d51f7826fe1d205bff00873b0005037fbed59
Amar <amarkhadkabardiya1234@gmail.com>
2026-04-11 01:28:02 +0545

    ready for deploy

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -125,7 +125,7 @@ const Work = () => {
                     rel="noopener noreferrer"
                     className="w-1/2 bg-purple-600 hover:bg-purple-800 text-white lg:px-6 lg:py-2 px-2 py-1 rounded-xl lg:text-xl text-sm font-semibold text-center"
                   >
-                    View Live
+                    Admin Panel
                   </a>
                 </div>
               </div>
